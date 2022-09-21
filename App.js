80908cd1f82fd4bfea54d5092ed16b37b9aeb8f7
Filip <113512133+FilipPijunovic@users.noreply.github.com>
2022-09-21 17:14:59 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Cart from './Cart';
+import './App.css';
+
+const items = [
+    { id: 1, name: 'Taco Seasoning', price: 2.25, qty: 2 },
+    { id: 2, name: 'Pinto Beans', price: 1.99, qty: 3 },
+    { id: 3, name: 'Sour Cream', price: 3.5, qty: 1 },
+];
+
+function App() {
+    return (
+        <div>
+            <Cart items={items} />
+        </div>
+    );
+}
+
+export default App;
